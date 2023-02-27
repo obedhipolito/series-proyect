ff8f7d6272cd1fedeea8f2588f7cdfb1e5bcb79b
obed <obedhipolito@gmail.com>
2023-02-27 13:34:55 -0600

    subiendo proyecto a master

---
+++ producciones/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Produccion(models.Model):
+    nombre = models.TextField(default='', blank=False)
+    temporadas = models.IntegerField(default=0, blank=False)
+    capitulos = models.IntegerField(default=0, blank=False)
+    duracion = models.IntegerField(default=0, blank=False)
+    clasificacion = models.TextField(default='', blank=False)
+    categoria = models.TextField(default='', blank=False)
+    idioma = models.TextField(default='', blank=False)
+    plataforma = models.TextField(default='', blank=False)
+    director = models.TextField(default='', blank=False)
+    protagonista = models.TextField(default='', blank=False)
