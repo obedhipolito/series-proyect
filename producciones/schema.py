3acd3d05a4100531fdb1f187cbf6b630eb606444
obed <obedhipolito@gmail.com>
2023-03-13 14:19:06 -0500

    subiendo mutations #2

--- producciones/schema.py
+++ producciones/schema.py
@@ -14,3 +14,54 @@ class Query(graphene.ObjectType):
 
     def resolve_producciones(self, info, **kwargs):
         return Produccion.objects.all()
+
+#tarea, cambiar por mis datos
+class CreateProduccion(graphene.Mutation):
+    id = graphene.Int()
+    nombre = graphene.String()
+    temporadas = graphene.Int()
+    capitulos = graphene.Int()
+    duracion = graphene.Int()
+    clasificacion = graphene.String()
+    categoria = graphene.String()
+    idioma = graphene.String()
+    plataforma = graphene.String()
+    director = graphene.String()
+    protagonista = graphene.String()
+
+    #2
+    class Arguments:
+        nombre = graphene.String()
+        temporadas = graphene.Int()
+        capitulos = graphene.Int()
+        duracion = graphene.Int()
+        clasificacion = graphene.String()
+        categoria = graphene.String()
+        idioma = graphene.String()
+        plataforma = graphene.String()
+        director = graphene.String()
+        protagonista = graphene.String()
+
+    #3
+    def mutate(self, info, nombre, temporadas, capitulos, duracion, clasificacion, categoria, idioma,plataforma, director, protagonista):
+        produciones = Produccion(nombre=nombre, temporadas=temporadas, capitulos=capitulos, duracion=duracion, clasificacion=clasificacion, categoria=categoria, idioma=idioma,plataforma=plataforma, director=director, protagonista=protagonista)
+        produciones.save()
+
+        return CreateProduccion(
+            id=produciones.id,
+            nombre=produciones.nombre,
+            temporadas=produciones.temporadas,
+            capitulos=produciones.capitulos,
+            duracion=produciones.duracion,
+            clasificacion=produciones.clasificacion,
+            categoria=produciones.categoria,
+            idioma=produciones.idioma,
+            plataforma=produciones.plataforma,
+            director=produciones.director,
+            protagonista=produciones.protagonista
+        )
+
+
+#4
+class Mutation(graphene.ObjectType):
+    create_producciones = CreateProduccion.Field()
